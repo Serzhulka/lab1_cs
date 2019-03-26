7137d1757169aed0ae595baa8a129c45a732b95a
Serzhulka <sergioilovesergio@gmail.com>
2019-03-26 10:16:03 +0200

    Lab1

---
+++ lab1_1_cs/lab1_1_cs/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace lab1_1_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = File.ReadAllText("D:\\labs_sys_prog\\lab1_cs\\text2.txt", Encoding.GetEncoding(1251));
+            var bits = string.Empty;
+            foreach (var character in s)
+            {
+                bits += Convert.ToString(character, 2).PadLeft(8, '0');
+            }
+
+            string base64 = string.Empty;
+
+            const byte threeOctets = 24;
+            var octetsTaken = 0;
+            while (octetsTaken < bits.Length)
+            {
+                var currentOctects = bits.Skip(octetsTaken).Take(threeOctets).ToList();
+
+                const byte sixBits = 6;
+                int hextetsTaken = 0;
+                while (hextetsTaken < currentOctects.Count())
+                {
+                    var chunk = currentOctects.Skip(hextetsTaken).Take(sixBits);
+                    hextetsTaken += sixBits;
+
+                    var bitString = chunk.Aggregate(string.Empty, (current, currentBit) => current + currentBit);
+
+                    if (bitString.Length < 6)
+                    {
+                        bitString = bitString.PadRight(6, '0');
+                    }
+                    var singleInt = Convert.ToInt32(bitString, 2);
+
+                    base64 += Base64Letters[singleInt];
+                }
+
+                octetsTaken += threeOctets;
+            }
+
+            // Pad with = for however many octects we have left
+            for (var i = 0; i < (bits.Length % 3); i++)
+            {
+                base64 += "=";
+            }
+            string writePath = @"D:\\labs_sys_prog\\lab1_cs\\text5.txt";
+            using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(base64);
+            }
+            Console.WriteLine(base64);
+            //return base64;
+            //var Bytes = System.Text.Encoding.UTF8.GetBytes("D:\\labs_sys_prog\\lab1_cs\\text.txt");
+            //be.EncodeFromFile(@"src/galera.txt", @"src/result.txt", Resources.StandardBase64Alphabet);
+            //Console.WriteLine(be.CheckCorrect(@"src/galera.txt", @"src/result.txt"));
+
+            ////Console.WriteLine(Base64Encode(s));
+        }
+        public static string Base64Encode(string s)
+        {
+            s = File.ReadAllText("D:\\labs_sys_prog\\lab1_cs\\text2.txt", Encoding.GetEncoding(1251));
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(s);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        private static readonly char[] Base64Letters = new[]
+        {  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9','+','/'};
+    }
+}
---
+++ lab1_cs/lab1_cs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab1_cs
+{
+    class Program
+    {
+        public static double ShannonEntropy(string s)
+        {
+            s = File.ReadAllText("D:\\labs_sys_prog\\lab1_cs\\text5.txt", Encoding.GetEncoding(1251));
+            var map = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!map.ContainsKey(c))
+                    map.Add(c, 1);
+                else
+                    map[c] += 1;
+            }
+
+            double result = 0.0;
+            int len = s.Length;
+            foreach (var item in map)
+            {
+                var frequency = (double)item.Value / len;
+                result -= frequency * (Math.Log(frequency) / Math.Log(2));
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            // 1.
+            // Array to store frequencies.
+            int[] c = new int[char.MaxValue];
+
+            // 2.
+            // Read entire text file.
+            string s = File.ReadAllText("D:\\labs_sys_prog\\lab1_cs\\text5.txt", Encoding.GetEncoding(1251));
+
+            // 3.
+            // Iterate over each character.
+            foreach (char t in s)
+            {
+                // Increment table.
+                c[t]++;
+            }
+
+            // 4.
+            // Write all letters found.
+
+            for (int i = 0; i < char.MaxValue; i++)
+            {
+                if (c[i] > 0 && char.IsLetterOrDigit((char)i))
+                {
+                    Console.WriteLine("Letter: {0}  Frequency: {1}", (char)i, c[i]);
+                    //Console.WriteLine(s);
+                }
+            }
+            long length = new System.IO.FileInfo("D:\\labs_sys_prog\\lab1_cs\\text5.txt").Length;
+            Console.WriteLine("Entropy: {0}", ShannonEntropy(s));
+            Console.WriteLine("Number of signs: {0}", s.Length);
+            Console.WriteLine("Amount of info: {0}", (s.Length * ShannonEntropy(s)) / 8);
+            Console.WriteLine("File Size: {0}", length);
+
+        }
+    }
+}
